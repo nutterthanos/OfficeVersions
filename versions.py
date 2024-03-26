97e0fba48d748e2f867c415e0d7fef8d8368821a
GitHub Action <action@github.com>
2024-03-26 18:22:23 +0000

    Updated Version's DB as of - 2024-03-26 18:22:23 UTC

--- versions.py
+++ versions.py
@@ -37,8 +37,8 @@ async def process_versions(session, semaphore, base_url, start_build, end_build,
 
 async def main():
     base_url = "http://officecdn.microsoft.com/sg/7D632000-B182-4E05-A91D-763D282C24DE/Office/Data/v64_{version}.cab"
-    start_build = 17100
-    end_build = 17200
+    start_build = 17200
+    end_build = 17300
     start_revision = 0
     end_revision = 25000
     output_file = "valid_urls.txt"
